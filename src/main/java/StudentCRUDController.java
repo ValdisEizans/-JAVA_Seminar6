2aa77a2995e76d9176953e6859177d5e8cbb469c
ValdisEizans <valdis.eizans@gmail.com>
2026-05-06 12:09:57 +0300

    Edit: controller added

---
+++ src/main/java/StudentCRUDController.java
@@ -0,0 +1,28 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lv.venta.service.IStudentCRUDService;
+
+@Controller
+@RequestMapping("/student/crud")
+public class StudentCRUDController {
+	
+	@Autowired
+	private IStudentCRUDService studCRUDService;
+	
+	@GetMapping("/all")//localhost:8080/student/crud/all
+	public String getAllStudents(Model model) {
+		try {
+			model.addAttribute("package", studCRUDService.retrieveAll());
+			return "show-all-students";
+		}
+		catch (Exception e) {
+			model.addAttribute("package", e.getMessage());
+			return "error-page";
+		}
+	}
+
+}
